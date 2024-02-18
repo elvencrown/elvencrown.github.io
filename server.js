9d2f0bb331f6cbdd5f6339ade2453114ecd667d0
SmithProgrammer <142420254+elvencrown@users.noreply.github.com>
2024-02-18 15:17:34 +0200

    v5685675

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', function (req, res) {
 });
 
 // Настройка порта для прослушивания
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log('Server started on port ' + port);
