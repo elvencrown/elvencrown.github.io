00548fb93364d9e48d6e8bacbd0be318c530a9b2
SmithProgrammer <142420254+elvencrown@users.noreply.github.com>
2024-02-26 02:28:31 +0200

    v1

--- ec-vue-start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d E:\KATYPERRY\Documents\GitHub\elvencrown.github.io
-
-call start cmd /k json-server --watch db.json --port 3000 --host=192.168.3.140
-timeout /t 5
-call start cmd /k npm run dev -- --host=192.168.3.140
--- src/mixins/newPostKeyMixin.js
+++
@@ -1,7 +0,0 @@
-export const newPostKeyMixin = {
-  data() {
-    return {
-      newPostKey: '87769457ef2c9ca15976ec4ebf25e064'
-    }
-  }
-}
